6d224cb212e232723a0f0711ffd0d82730545e62
Tuấn Nguyễn <tuannguyen@Tuans-MacBook-Pro.local>
2024-07-31 00:05:58 +0700

    Build UI job form

---
+++ src/app/components/JobForm.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import {
+  faEnvelope,
+  faPhone,
+  faStar,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Theme,
+  TextField,
+  RadioGroup,
+  Button,
+  TextArea,
+} from "@radix-ui/themes";
+import { useState } from "react";
+import {
+  CitySelect,
+  CountrySelect,
+  StateSelect,
+  LanguageSelect,
+} from "react-country-state-city";
+import "react-country-state-city/dist/react-country-state-city.css";
+
+export default function JobForm() {
+  const [countryid, setCountryid] = useState(0);
+  const [stateid, setstateid] = useState(0);
+  return (
+    <Theme>
+      <form action="" className="container mt-6 flex flex-col gap-4">
+        <TextField.Root placeholder="Job title"></TextField.Root>
+
+        <div className="grid grid-cols-3 gap-6">
+          <div>
+            Remote?
+            <RadioGroup.Root defaultValue="onsite" name="example">
+              <RadioGroup.Item value="onsite">On-site</RadioGroup.Item>
+              <RadioGroup.Item value="hybrid">Hybrid-remote</RadioGroup.Item>
+              <RadioGroup.Item value="remote">Fully remote</RadioGroup.Item>
+            </RadioGroup.Root>
+          </div>
+          <div>
+            Full time?
+            <RadioGroup.Root defaultValue="project" name="example">
+              <RadioGroup.Item value="project">Project</RadioGroup.Item>
+              <RadioGroup.Item value="part">Part-time</RadioGroup.Item>
+              <RadioGroup.Item value="full">Full-time</RadioGroup.Item>
+            </RadioGroup.Root>
+          </div>
+          <div>
+            Salary
+            <TextField.Root>
+              <TextField.Slot>$</TextField.Slot>
+              <TextField.Slot>k/year</TextField.Slot>
+            </TextField.Root>
+          </div>
+        </div>
+        <div>
+          Location
+          <div className="flex gap-4 *:grow">
+            <CountrySelect
+              onChange={(e) => {
+                setCountryid(e.id);
+              }}
+              placeHolder="Select Country"
+            />
+            <StateSelect
+              countryid={countryid}
+              onChange={(e) => {
+                setstateid(e.id);
+              }}
+              placeHolder="Select State"
+            />
+            <CitySelect
+              countryid={countryid}
+              stateid={stateid}
+              //   onChange={(e) => {
+              //     console.log(e);
+              //   }}
+              placeHolder="Select City"
+            />
+          </div>
+        </div>
+        <div className="flex gap-6">
+          <div className="w-1/3">
+            <h3>Job icon</h3>
+            <div className="bg-gray-100 rounded-md size-24 inline-flex items-center content-center justify-center">
+              <FontAwesomeIcon icon={faStar} className="text-gray-400" />
+            </div>
+            <div className="mt-2">
+              <Button variant="soft">Select file</Button>
+            </div>
+          </div>
+          <div className="grow">
+            <h3>Contact person</h3>
+            <div className="flex gap-4">
+              <div className="flex flex-col ">
+                <div className="bg-gray-100 rounded-md size-24 inline-flex items-center content-center justify-center">
+                  <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+                </div>
+                <div className="mt-2">
+                  <Button variant="soft">Select file</Button>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 grow">
+                <TextField.Root placeholder="John Doe">
+                  <TextField.Slot>
+                    <FontAwesomeIcon icon={faUser} />
+                  </TextField.Slot>
+                </TextField.Root>
+                <TextField.Root placeholder="Phone" type="tel">
+                  <TextField.Slot>
+                    <FontAwesomeIcon icon={faPhone} />
+                  </TextField.Slot>
+                </TextField.Root>
+                <TextField.Root placeholder="Email" type="email">
+                  <TextField.Slot>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </TextField.Slot>
+                </TextField.Root>
+              </div>
+            </div>
+          </div>
+        </div>
+        <TextArea placeholder="Job description" />
+        <div className="flex justify-center">
+          <Button size="3">
+            <span className="px-8">Save</span>
+          </Button>
+        </div>
+      </form>
+    </Theme>
+  );
+}
